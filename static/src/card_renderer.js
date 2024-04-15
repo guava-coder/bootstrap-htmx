b8eb23235f3fdc5789f119f746ea9efb2e4a7d8e
Wang-Kai-Chi <ericwangcatch@gmail.com>
2024-04-15 11:52:38 +0800

    update static/src/card_renderer.js

--- static/src/card_renderer.js
+++ static/src/card_renderer.js
@@ -5,7 +5,7 @@
  * @param {Element} cardResult - The element to which the rendered card elements will be appended.
  * @return {void}
  */
-export default function CardRenderer (value = [{ Title: 'Product01', Sub: 999 }], cardResult = Element) {
+export default function CardRenderer (value = [{ Title: 'content01', Sub: 999 }], cardResult = Element) {
   const cards = () => {
     let temp = ''
     for (const i in value) {
@@ -17,16 +17,13 @@ export default function CardRenderer (value = [{ Title: 'Product01', Sub: 999 }]
 }
 
 /**
- * Generate HTML for a card element based on product information.
+ * Generate HTML for a card element based on content information.
  *
- * @param {{
- *    Product_title: string,
- *    Price: number
- * }} product - The product information to display on the card
+ * @param {{ Title: string, Sub: number }} content - The product information to display on the card
  * @param {number} index - The index of the card
  * @return {string} The HTML string for the card element
  */
-function CardHTML (product = { Title: '', Sub: 0 }, index = 0) {
+function CardHTML (content = { Title: '', Sub: 0 }, index = 0) {
   const valueId = `v${index}`
   return /* html */`
         <div class="card border-info" id="card${valueId}">
@@ -34,15 +31,44 @@ function CardHTML (product = { Title: '', Sub: 0 }, index = 0) {
             <div class="d-flex align-items-center">
             <i class="bi bi-bag-fill" style="font-size: 2em;"></i>
                     <div class="ms-3 name me-auto">
-                        <div id="${valueId}" hidden>${JSON.stringify(product)}</div>
-                        <h5 class="font-bold">${product.Product_title}</h5>
-                        <h6 class="text-muted mb-0">${product.Price}</h6>
+                        <div id="${valueId}" hidden>${JSON.stringify(content)}</div>
+                        <h5 class="font-bold">${content.Product_title}</h5>
+                        <h6 class="text-muted mb-0">${content.Price}</h6>
                     </div>
-                    <i class="bi bi-three-dots-vertical" style="font-size: 2em;"></i>
+                ${CardDropUp(valueId)}
                 </div>
             </div>
         </div>
         </div>
         <div class="p-2 g-col-6"></div>
     `
+}
+
+/**
+ * Generate the HTML for a dropup card element with dropdown options for editing and deleting.
+ *
+ * @param {string} id - The identifier for the card element
+ * @return {string} The HTML string for the dropup card element
+ */
+function CardDropUp (id = '') {
+  return /* html */`
+  <div class="dropup">
+  <button class="btn" data-bs-toggle="dropdown" aria-expanded="false">
+      <i class="bi bi-three-dots-vertical" style="font-size: 2em;"></i>
+      <ul class="dropdown-menu">
+        <li>
+          <a class="dropdown-item" onclick="console.log('edit ${id}')">
+            <i class="bi bi-pen-fill text-primary" style="font-size: 2em;"></i>
+            Edit
+          </a>
+        </li>
+        <li>
+          <a class="dropdown-item" onclick="console.log('delete ${id}')">
+            <i class="bi bi-trash-fill text-danger" style="font-size: 2em;"></i>
+            Delete
+          </a>
+        </li>
+      </ul>
+  </div>
+  `
 }
